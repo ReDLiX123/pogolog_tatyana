5611b3e7194f5c1497a16a05bd04db1ce4fb9fcb
ReDLiX123 <andrej.krekhov@yandex.ru>
2026-08-10 19:24:01 +0800

    Updates

--- _next/static/ofiR4g7nJpgE05H-Cj9RQ/_buildManifest.js
+++ _next/static/ofiR4g7nJpgE05H-Cj9RQ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pogolog_tatyana//_next/:path+",
+        "destination": "/pogolog_tatyana/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
